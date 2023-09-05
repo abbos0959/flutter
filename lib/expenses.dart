db61cbe4cc43e733e26e9221f43a9c2172ce2485
abbos0959 <abbosakromovich777@gmail.com>
2023-09-05 08:47:34 +0500

    start

---
+++ lib/expenses.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Expenses extends StatelessWidget {
+  const Expenses({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Column(children: [Text("data")]),
+    );
+  }
+}
